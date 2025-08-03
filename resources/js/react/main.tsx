6b9e0cb945820448acc5939aa3fbd9488fd7918a
gunturmaulanap <gunturmaulanaputraprojo@gmail.com>
2025-08-03 17:47:38 +0700

    gemini ai

--- resources/js/react/main.tsx
+++ resources/js/react/main.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { AuthProvider } from "./hooks/useAuth";
 
 // Get the root element for React app
-const rootElement = document.getElementById('react-root');
+const rootElement = document.getElementById("react-root");
 
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <React.StrictMode>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </React.StrictMode>
   );
 }
